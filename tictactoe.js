0e658aa9cb8db37f63f03096fad30080f964d795
jl232367 <48839916+jl232367@users.noreply.github.com>
2021-05-05 15:48:45 -0400

    not sure what the fuck is going on, fuck it all.

--- tictactoe.js
+++ tictactoe.js
@@ -1,15 +1,22 @@
+
+
+
 const gameBoard = (() => {
     const setBoard = () => {
         for (let i = 1; i<= 9; i++) {
             //const board = document.querySelector(".gameboard");
             const boardSquare = document.getElementById("gb" + i);
             boardSquare.addEventListener('click', (e) => {
-                if (players.currentPlayer () = playerOne) {
+                if (players.currentPlayer = players.playerOne) {
                     const currentSquare = e.target.getAttribute("id");
                     currentSquare.innerHTML = "X!"
+                    console.log("The 'if' executed")
+                    players.switchPlayer(players.currentPlayer); 
                  } else {
                     const currentSquare = e.target.getAttribute("id");
                     currentSquare.innerHTML = "O!"
+                    console.log("the 'else' executed")
+                    players.switchPlayer(players.currentPlayer);
                 }
                 });
         }
@@ -22,14 +29,16 @@ const gameBoard = (() => {
 const players = ()  => {
  const playerOne = document.getElementById("player1").value;
  const playerTwo = document.getElementById("player2").value;
- const currentPlayer = () => {
-     if (currentPlayer = playerOne) {
-         currentPlayer = playerTwo;
+ const currentPlayer = playerOne
+ const switchPlayer = cp => {
+     if (cp = playerOne) {
+         cp = playerTwo;
      } else {
-         currentPlayer = playerOne;
+         cp = playerOne;
      }
+     return currentPlayer = cp; 
  };
- return {currentPlayer}
+ return {currentPlayer, switchPlayer, playerOne, playerTwo}
 };
 
 const game = () => {
